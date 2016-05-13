b8e6ff5ea02b6015fed3ee34d5b8e47a2769ad15
JenWestfallen <chicago.author@yahoo.com>
2016-05-12 21:28:27 -0500

    restful routing

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,29 @@
+class ProductsController < ApplicationController
+  def all
+    @products = Product.all
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product=Product.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @product = Product.create(
+      name: params[:name],
+      price: params[:price],
+      image: params[:image],
+      description: params[:description]
+      )
+    render 'show.html.erb'
+  end
+  
+end
+
+
