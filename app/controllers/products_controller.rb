986fb7e31ab2cad0799463ee9ff34d8f80dd4604
JenWestfallen <chicago.author@yahoo.com>
2016-05-15 17:08:16 -0500

    styling

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,9 +21,40 @@ class ProductsController < ApplicationController
       image: params[:image],
       description: params[:description]
       )
-    render 'show.html.erb'
+
+    flash[:success]="Recipe Created"
+
+    redirect_to "/products/#{@product.id}"
   end
   
+  def edit
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(
+      name: params[:name],
+      price: params[:price],
+      image: params[:image],
+      description: params[:description]
+      )
+
+     flash[:success]="Product Updated"
+
+      redirect_to "/products/#{@product.id}"
+    end
+
+    def destroy
+      @product = Product.find(params[:id])
+
+      @product.destroy
+      flash[:warning] = "Product Deleted"
+
+      redirect_to "/"
+      
+    end
+
 end
 
 
